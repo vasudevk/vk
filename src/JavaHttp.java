92efb3f74413e8688d4a6c85a741f84175000472
vasu <devkoneru@outlook.com>
2023-01-01 21:00:57 -0800

    Http with JDK

---
+++ src/JavaHttp.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class JavaHttp {
+    public static void main(String[] args) {
+        try {
+            var url = "https://jsonplaceholder.typicode.com/albums";
+            var method = "GET";
+            fetchStream(url, method);
+            System.out.println(" ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ");
+            fetchClient(url);
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void fetchStream(String uri, String method) throws IOException {
+        URL url = new URL(uri);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod(method);
+        conn.setConnectTimeout(5000);
+        conn.setReadTimeout(5000);
+
+        StringBuilder response = new StringBuilder();
+        if (200 == conn.getResponseCode()) {
+            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String line;
+            while (null != (line = br.readLine())) {
+                response.append(line);
+            }
+            br.close();
+            conn.disconnect();
+            System.out.println(response);
+        }
+    }
+
+    public static void fetchClient(String uri) throws IOException, InterruptedException {
+        var client = HttpClient.newHttpClient();
+        var request = HttpRequest.newBuilder().uri(URI.create(uri)).build();
+//        client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+//                .thenApply(HttpResponse::body)
+//                .thenAccept(System.out::println)
+//                .join();
+        var response = client.send(request, HttpResponse.BodyHandlers.ofString()).body();
+        System.out.println(response);
+    }
+}
